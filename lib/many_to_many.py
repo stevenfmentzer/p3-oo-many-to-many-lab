d083f06aab665678c0531cc968f8d8f87837f35a
Steven Mentzer <stevenfmentzer@gmail.com>
2024-01-15 22:36:11 -0500

    12/14 passing tests, test.py is outdated

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,51 @@
 class Author:
-    pass
+    all = []
+    def __init__(self,name):
+        if not isinstance(name, str):
+            raise Exception("name must be type 'string'")
+        else: 
+            self.name = name
+            self.__class__.all.append(self)
 
+    def contracts(self):
+        print(self.name)
+        print([contract.author for contract in Contract.all])
+        return [contract for contract in Contract.all if contract.author == self]
+
+
+    def books(self):
+        return [contract.book for contract in Contract.all if contract.author == self]
+
+    def sign_contract(self, book, date, royalties): 
+        if not isinstance(book, Book) or not isinstance(date, str) or not isinstance(royalties, int):
+            raise Exception("Invalid contract parameters")
+        new_contract = Contract(self, book, date, royalties)
+        return new_contract
+
+    def total_royalties(self):
+        return sum(contract.royalties for contract in Contract.all if contract.author == self)
 
 class Book:
-    pass
-
+    all = []
+    def __init__(self, title):
+        if not isinstance(title, str):
+            raise Exception("title must be type 'string'")
+        self.title = title
+        self.__class__.all.append(self)
 
 class Contract:
-    pass
+    all = []
+
+    def __init__(self, author, book, date, royalties):
+        if not isinstance(author, Author) or not isinstance(book, Book) or not isinstance(date, str) or not isinstance(royalties, int):
+            raise Exception("Invalid contract parameters")
+        else:  
+            self.author = author
+            self.book = book
+            self.date = date
+            self.royalties = royalties
+            self.__class__.all.append(self)
+
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [contract for contract in cls.all if contract.date == date]
